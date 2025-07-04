269b5e665912ed210bd364f3cc451193ce565601
Menwuyelet <menutemesgen@gmail.com>
2025-07-04 17:09:04 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -3,3 +3,7 @@ CREATE INDEX idx_property_name ON Property (name);
 CREATE INDEX idx_property_location ON Property (location);
 
 CREATE INDEX idx_booking_start_date ON Booking (start_date);
+
+EXPLAIN ANALYZE SELECT * FROM Property WHERE name = 'Lake View';
+
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE start_date >= '2025-07-01';
