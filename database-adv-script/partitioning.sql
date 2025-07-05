0941e43bb1eb627bd1780f5c6f8885fff87a68c2
Menu Temesgen <menutemesgen@gmail.com>
2025-07-05 15:06:43 +0300

    -- updated the partitioning.sql file to meet requirments.

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,3 +1,14 @@
+ALTER TABLE Booking
+PARTITION BY RANGE (YEAR(start_date)) (
+    PARTITION g2023 VALUES LESS THAN (2024),
+    PARTITION g2024 VALUES LESS THAN (2025),
+    PARTITION g2025 VALUES LESS THAN (2026),
+    PARTITION gmax VALUES LESS THAN MAXVALUE
+);
+
+-- the above script does not work on mysql since there are foreign keys on the booking table, and my sql
+-- doesn`t support partitioning on tables containing foreign keys. so i also added the below script which works fine on PostgreSQL.
+
 CREATE TABLE booking_2023 PARTITION OF Booking FOR
 VALUES
 FROM ('2023-01-01') TO ('2024-01-01');
